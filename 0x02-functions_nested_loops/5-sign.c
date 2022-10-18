65cac109233790accc6d51d4b84697f33c3f95d3
titanckw <karikuclemo@gmail.com>
2022-10-18 15:05:09 +0000

    Prints sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,21 +4,24 @@
  * print_sign - Prints sign of a number
  * @n: prints positive or negative number
  *
- * Return: Always 1 if n>0, 0 n\\==0, -1<0
+ * Return: Always 1 if n>0, 0 n==0, -1<0
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
+		_putchar('-');
 		return (-1);
 	}
 }
